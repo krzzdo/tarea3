32e8ab7007797c50cc10f29798a7096fb541d224
pancho <franciscgmd@gmail.com>
2025-06-17 23:13:30 -0400

    Update tarea3.pl
    creados todos los writes de las opciones del menu

--- tarea3.pl
+++ tarea3.pl
@@ -1,9 +1,12 @@
+% comienzo del programa
 main :-
     write('=== SISTEMA NUTRICIONAL ==='),nl,
     menu.
 
+% prints del menu
 menu :-
     repeat,
+    nl, write('--- SISTEMA NUTRICIONAL ---'), nl,
     write('1. Calcular peso ideal por altura'), nl,
     write('2. Calcular IMC tradicional'), nl,
     write('3. Calcular IMC por edad y sexo'), nl,
@@ -14,4 +17,34 @@ menu :-
      Opcion == 3 -> imcEdadSexo, fail;
      Opcion == 4 -> write('Saliendo del programa... ¡Hasta pronto!'), !, fail;
      write('Opcion invalida. Intente de nuevo'), nl, fail).
-    
+
+% prints y tomas de valores de peso ideal por altura.
+pesoIdealAltura :-
+    nl, write('--- PESO IDEAL POR ALTURA ---'), nl,
+    write('Ingrese altura en cm (144-191): '),
+    read(Altura),
+    write('Ingrese sexo (hombre/mujer): '),
+    read(Sexo),
+    write('RESULTADO:').
+
+% prints imc tradicional y toma de valores.
+imcTradicional :-
+    nl, write('--- IMC TRADICIONAL ---'), nl,
+    write('Ingrese peso en kg: '),
+    read(PesoKG),
+    write('Ingrese altura en cm: '),
+    read(Altura),
+    write('RESULTADO').
+
+% prints de imc por edad y sexo, y toma de valores
+imcEdadSexo :-
+    nl, write('--- IMC POR EDAD Y SEXO ---'), nl,
+    write('Ingrese peso en kg: '),
+    read(PesoKG),
+    write('Ingrese altura en cm: '),
+    read(Altura),
+    write('Ingrese edad (2-100): '),
+    read(Edad),
+    write('Ingrese sexo (hombre/mujer): '),
+    read(Sexo),
+    write('RESULTADO').
